4c406483c57bba9d9841249159ca6132dc120094
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-09-11 22:04:43 +0530

    Talwind in react

---
+++ vite-project/src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/button.css'
+
+const Button = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded">This is a Button</div>
+  )
+}
+
+export default Button
