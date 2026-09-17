d34c3b1853617b63655e9c5920af36b84f335370
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-09-17 13:45:15 +0530

    UseState in Hooks

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import UseStates from './components/hooks/UseStates'
 
 
 const App = () => {
@@ -6,7 +7,7 @@ const App = () => {
   
   return (
     <>
-      
+     <UseStates/> 
     </> 
   
   )
