1fa92f6d5f98b57961894df3b7e99b6c958398d4
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-09-18 22:18:08 +0530

    UseState in Hooks

--- vite-project/src/components/hooks/UseStates.jsx
+++ vite-project/src/components/hooks/UseStates.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UseStates = () => {
+  const [a, seta] = useState(0);
+  const inc=()=>{
+    seta(a+1);
+  }
+
+  const dec=()=>{
+    seta(a-1);
+  }
   return (
     <>
-    hii
+    <h1>{a}</h1>
+    <button onClick={inc}>Increase</button>
+    <button onClick={dec}>Decrease</button>
+
     </>
   )
 }
